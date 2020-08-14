3f8f383902fe9d9b3c4740a738ab0ebf8e4caef0
Ivan Filatov <missed94@gmail.com>
2020-08-14 14:43:18 +0700

    сделал виджет со скиллами и слайдер с отзывами

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,45 @@
-console.log('this is the skills module');
+import Vue from "vue";
+
+const skillsItem = {
+  props: ["skill"],
+  template: "#skills-item",
+  methods: {
+    drawingPrecentCircle() {
+      const circle = this.$refs["colored-circle"];
+      const dashArray = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      );
+      const precent = (dashArray / 100) * (100 - this.skill.precent);
+      circle.style.strokeDashoffset = precent;
+    },
+  },
+  mounted() {
+    this.drawingPrecentCircle();
+  },
+};
+
+const skillsRow = {
+  props: ["category"],
+  template: "#skills-row",
+  components: {
+    skillsItem,
+  },
+};
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  components: {
+    skillsRow,
+  },
+
+  data() {
+    return {
+      skills: [],
+    };
+  },
+
+  created() {
+    this.skills = require("../data/skills.json");
+  },
+});
