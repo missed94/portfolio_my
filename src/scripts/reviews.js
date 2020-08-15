26a0637ed069c15deefe3855c9998092a6ead898
Ivan Filatov <missed94@gmail.com>
2020-08-15 15:48:06 +0700

    закончил слайдер с работами

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,44 +1,50 @@
 import Vue from "vue";
-import { Swiper, SwiperSlide} from 'vue-awesome-swiper'
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 // If you use Swiper 6.0.0 or higher
-import 'swiper/swiper-bundle.css'
+import "swiper/swiper-bundle.css";
 
 new Vue({
   el: "#slider-component",
   template: "#slider-container",
   components: {
-    Swiper, SwiperSlide
+    Swiper,
+    SwiperSlide,
   },
   data() {
     return {
       reviews: [],
       sliderOptions: {
         slidesPerView: 1,
-      }
+        breakpoints: {
+          // when window width is >= 320px
+          769: {
+            slidesPerView: 2,
+          },
+          // when window width is >= 480px
+        },
+      },
     };
   },
   methods: {
     requireImagesToArray(data) {
       return data.map((item) => {
-        const requiredImage = require(`../images/content/${item.pic}`)
-        .default;
+        const requiredImage = require(`../images/content/${item.pic}`).default;
         item.pic = requiredImage;
         return item;
       });
     },
 
     slide(direction) {
-        const slider = this.$refs["slider"].$swiper;
-        switch(direction) {
-            
-            case "next":
-                slider.slideNext()
-                break;
-            case "prev":
-                slider.slidePrev()
-                break;    
-        }
-    }
+      const slider = this.$refs["slider"].$swiper;
+      switch (direction) {
+        case "next":
+          slider.slideNext();
+          break;
+        case "prev":
+          slider.slidePrev();
+          break;
+      }
+    },
   },
 
   created() {
