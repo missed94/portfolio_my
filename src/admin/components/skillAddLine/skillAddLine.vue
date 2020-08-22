928686bb922fb709c178531ed83f263b45ef8b34
Ivan Filatov <missed94@gmail.com>
2020-08-22 18:33:40 +0700

    admin

--- src/admin/components/skillAddLine/skillAddLine.vue
+++ src/admin/components/skillAddLine/skillAddLine.vue
@@ -27,7 +27,7 @@
 <script>
 import input from "../input";
 //import button from "../button";
-import roundBtn from "../button/";
+import button from "../button/";
 
 
 export default {
@@ -37,7 +37,7 @@ export default {
 
   components: {
     appInput: input,
-    roundBtn
+    roundBtn:button
   }
 };
 </script> 
